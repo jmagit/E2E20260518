41865f54d9606774a9ef1d34665bf960df9861d9
Javier <35864946+jmagit@users.noreply.github.com>
2026-05-18 17:26:13 +0200

    configuracion

---
+++ curso/eslint.config.mjs
@@ -0,0 +1,14 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import json from "@eslint/json";
+import { defineConfig } from "eslint/config";
+import playwright from 'eslint-plugin-playwright'
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
+  tseslint.configs.recommended,
+  { files: ['tests/**'], extends: [playwright.configs['flat/recommended']],     rules: { "no-unused-vars": "off", }, },
+]);
